e889f9895ae96708d21ba7786dc30451fb48cedc
b4rt4s <bartek.baut35@gmail.com>
2024-08-26 23:02:20 +0200

    fix: improve proxy handling and JSON formatting

--- usos-abstracts-scraper/usos-abstracts-scraper.py
+++ usos-abstracts-scraper/usos-abstracts-scraper.py
@@ -1,52 +1,65 @@
 from fp.fp import FreeProxy
 from lxml import html
+from fake_useragent import UserAgent
 from random import randint
 from time import sleep
 import requests
 import json
 import os
 
+ua = UserAgent()
+
 proxy = FreeProxy(rand=True, anonym=True).get()
 
 proxies = {
-    "http": proxy,
-    "https": proxy
+    'http': proxy
 }
-
-print(proxies)
 
 current_directory = os.getcwd()
 
 if 'usos-abstracts-scraper' in os.path.basename(current_directory):
-    file_path = os.path.join(current_directory, 'thesis_data.json')
+    file_path = os.path.join(current_directory, 'theses_data.json')
 else:
-    file_path = os.path.join(current_directory, 'usos-abstracts-scraper', 'thesis_data.json')
+    file_path = os.path.join(current_directory, 'usos-abstracts-scraper', 'theses_data.json')
 
-with open(file_path, "a") as file:
-    #file.write("[") 
-    for thesis_id in range(1, 100): # max 16940 
-        url = f"https://apd.usos.pwr.edu.pl/diplomas/{thesis_id}"
+thesis_No = 0
 
-        response = requests.get(url, proxies=proxies, timeout=10, headers={"User-Agent":"Mozilla/5.0", "Accept": "application/json"})
+first_thesis_id = int(input('Input initial thesis id - minimum 1: '))
+last_thesis_id = int(input('Input final thesis id - maximum 16946 as of 26/08/2024: ')) 
 
-        print(response)
+if first_thesis_id == last_thesis_id:
+    last_thesis_id += 1
 
-        tree = html.fromstring(response.content)
+theses_list = []
 
-        thesis_data = {
-            "thesis_id": thesis_id,
-            "pl_title": ' '.join(tree.xpath("//*[@id=\"thesisInfo\"]/tbody/tr[2]/td[2]/div/div[1]/div[2]/text()")).strip(),
-            "en_title": ' '.join(tree.xpath("//*[@id=\"thesisInfo\"]/tbody/tr[2]/td[2]/div/div[2]/div[2]/text()")).strip(),
-            "thesis_promotor": ' '.join(tree.xpath("//*[@id=\"thesisInfo\"]/tbody/tr[4]/td[2]/div/span/a/text()")).strip(),
-            "pl_abstract": ' '.join(tree.xpath("//*[@id=\"thesisInfo\"]/tbody/tr[8]/td[2]/div/div[1]/div[2]/text()")).strip(),
-            "en_abstract": ' '.join(tree.xpath("//*[@id=\"thesisInfo\"]/tbody/tr[8]/td[2]/div/div[2]/div[2]/text()")).strip(),
-            "pl_keywords": ' '.join(tree.xpath("//*[@id=\"thesisInfo\"]/tbody/tr[9]/td[2]/div/div[1]/div[2]/text()")).strip(),
-            "en_keywords": ' '.join(tree.xpath("//*[@id=\"thesisInfo\"]/tbody/tr[9]/td[2]/div/div[2]/div[2]/text()")).strip()
-        }
-        if thesis_id > 1:
-            file.write(",")
-            
-        json.dump(thesis_data, file, indent=6)
+for thesis_id in range(first_thesis_id, last_thesis_id): 
+    url = f'https://apd.usos.pwr.edu.pl/diplomas/{thesis_id}'
+
+    # generate fake User-Agents
+    fake_ua = ua.random
+
+    response = requests.get(url, proxies=proxies, timeout=5, headers={'User-Agent': fake_ua, 'Accept': 'application/json'})
+
+    thesis_No += 1
+
+    print(thesis_No, response)
+
+    tree = html.fromstring(response.content)
+
+    thesis_data = {
+        'thesis_id': thesis_id,
+        'pl_title': ' '.join(tree.xpath('//*[@id=\'thesisInfo\']/tbody/tr[2]/td[2]/div/div[1]/div[2]/text()')).strip(),
+        'en_title': ' '.join(tree.xpath('//*[@id=\'thesisInfo\']/tbody/tr[2]/td[2]/div/div[2]/div[2]/text()')).strip(),
+        'thesis_promotor': ' '.join(tree.xpath('//*[@id=\'thesisInfo\']/tbody/tr[4]/td[2]/div/span/a/text()')).strip(),
+        'pl_abstract': ' '.join(tree.xpath('//*[@id=\'thesisInfo\']/tbody/tr[8]/td[2]/div/div[1]/div[2]/text()')).strip(),
+        'en_abstract': ' '.join(tree.xpath('//*[@id=\'thesisInfo\']/tbody/tr[8]/td[2]/div/div[2]/div[2]/text()')).strip(),
+        'pl_keywords': ' '.join(tree.xpath('//*[@id=\'thesisInfo\']/tbody/tr[9]/td[2]/div/div[1]/div[2]/text()')).strip(),
+        'en_keywords': ' '.join(tree.xpath('//*[@id=\'thesisInfo\']/tbody/tr[9]/td[2]/div/div[2]/div[2]/text()')).strip()
+    }
         
-        sleep(randint(5,10))
-    #file.write("]")
+    theses_list.append(thesis_data)
+
+    sleep(randint(2,5))
+
+with open(file_path, 'a') as file:
+    json.dump(theses_list, file, sort_keys=True, indent=4, separators=(',', ': '))
